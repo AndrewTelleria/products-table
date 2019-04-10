1bd0f7ef30bb5ef55980a265ac63576d35a6ca84
Andrew K Telleria <andrew@aktelleria.com>
2019-04-10 10:58:49 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,108 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class ProductCategoryRow extends React.Component {
+  render() {
+    const category = this.props.category;
+    return (
+    <tr>
+      <th colSpan="2">
+        {category}
+      </th>
+    </tr>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class ProductRow extends React.Component {
+  render() {
+    const product = this.props.name;
+    const name = product.stocked ? product.name :
+      <span style={{color: 'red'}}>
+        {product.name}
+      </span>;
+
+    return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+    );
+  }
+}
+
+class ProductTable extends React.Component {
+  render() {
+    const rows = [];
+    let lastCategory = null;
+
+    this.props.products.forEach((product) => {
+      if (product.category !== lastCategory) {
+        rows.push(
+          <ProductCategoryRow
+           category={product.category}
+           key={product.category} />
+        );
+      }
+      rows.push(
+        <ProductRow
+          name={product}
+          key={product.name} />
+      );
+      lastCategory = product.category;
+    });
+
+    return(
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+}
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Search..." />
+        <p>
+          <input type="checkbox" />
+          {' '}
+          Only show products in stock
+        </p>
+      </form>
+    );
+  }
+}
+
+class FilterableProductTable extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <ProductTable products={products}/>
+      </div>
+    );
+  }
+}
+
+const products = [
+  {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+  {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+  {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+  {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+  {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+  {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+];
+
+ReactDOM.render(
+  <FilterableProductTable />,
+  document.getElementById('root'));
+
